4e94ba45d82e201c8c20784300a78d04aaa0481d
KailinLi <likailinhust@gmail.com>
2017-11-03 21:48:49 +0800

    tmp finish

--- QClient/rdtsendersocket.cpp
+++ QClient/rdtsendersocket.cpp
@@ -32,18 +32,34 @@ void RdtSenderSocket::sendFile(volatile qint64 *base, volatile qint64 *nextSeqnu
 void RdtSenderSocket::timeOut(volatile qint64 *base, volatile qint64 *nextSeqnum, volatile qint64 *N)
 {
     qDebug() << "call timeout";
+
     file->seek (*base);
-    for (qint64 index = *base; index < *nextSeqnum; index += SENDSIZE) {
+    *nextSeqnum = *base;
+    while (*nextSeqnum < *base + *N * SENDSIZE && *nextSeqnum < totalSize) {
         QDataStream stream(&outBlock, QIODevice::WriteOnly);
         stream.setVersion (QDataStream::Qt_5_6);
-        qint64 size = (SENDSIZE > (totalSize - index)) ? (totalSize - index) : SENDSIZE;//qMin(sendSize, bytesNotWrite);
+        qint64 size = (SENDSIZE > (totalSize - *nextSeqnum)) ? (totalSize - *nextSeqnum) : SENDSIZE;//qMin(sendSize, bytesNotWrite);
         stream << size;
-        stream << index;
+        stream << *nextSeqnum;
         stream << file->read (size);
         write (outBlock.constData (), outBlock.size ());
         outBlock.resize (0);
-        index += size;
+
+        *nextSeqnum += size;
+
     }
+
+//    for (qint64 index = *base; index < *nextSeqnum; index += SENDSIZE) {
+//        QDataStream stream(&outBlock, QIODevice::WriteOnly);
+//        stream.setVersion (QDataStream::Qt_5_6);
+//        qint64 size = (SENDSIZE > (totalSize - index)) ? (totalSize - index) : SENDSIZE;//qMin(sendSize, bytesNotWrite);
+//        stream << size;
+//        stream << index;
+//        stream << file->read (size);
+//        write (outBlock.constData (), outBlock.size ());
+//        outBlock.resize (0);
+//        index += size;
+//    }
 }
 
 void RdtSenderSocket::deleteSelf()
